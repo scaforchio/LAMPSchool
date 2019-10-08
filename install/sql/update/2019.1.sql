b9aecb5c0c0214883308a4c16eb02d9d6bb7d6de
scaforchio <pietro.tamburrano@gmail.com>
2019-10-08 08:42:27 +0200

    Aumento dimensione campi entrata posticipata e uscita aunticipata.

---
+++ install/sql/update/2019.1.sql
@@ -0,0 +1,8 @@
+--
+-- Aggiornamento di LAMPSchool alla versione 2019.1
+--
+ALTER TABLE tbl_alunni CHANGE autentrata autentrata VARCHAR(100) NULL DEFAULT NULL, 
+                       CHANGE autuscita autuscita VARCHAR(100) NULL DEFAULT NULL;
+-- LASCIARE SEMPRE ALLA FINE
+UPDATE tbl_parametri set valore='2019.1' where parametro='versioneprecedente';
+-- LASCIARE SEMPRE ALLA FINE
