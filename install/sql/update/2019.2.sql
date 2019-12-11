78a261e32c0b57f7ad748adb05a941e6cfc1676c
scaforchio <pietro.tamburrano@gmail.com>
2019-12-11 09:15:28 +0100

    Aggiunto indice univoco su assenze per evitare doppio inserimento in caso di simultaneità di invio da parte di più terminali marcatempo.

---
+++ install/sql/update/2019.2.sql
@@ -0,0 +1,13 @@
+--
+-- Aggiornamento di LAMPSchool alla versione 2019.1
+--
+
+
+-- Aggiunto indice univoco per evitare doppio inserimento assenza in caso di simultaneità invio 
+-- da parte di più terminali marcatempo
+
+ALTER TABLE tbl_assenze ADD UNIQUE(idalunno, data);
+
+-- LASCIARE SEMPRE ALLA FINE
+UPDATE tbl_parametri set valore='2019.2' where parametro='versioneprecedente';
+-- LASCIARE SEMPRE ALLA FINE
