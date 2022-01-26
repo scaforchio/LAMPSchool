77efff7d860eb86af7374f51023a87e85a2c0716
pietro <pietro@pietro-Aspire-A515-51G>
2022-01-26 08:40:32 +0100

    Assenze orarie automaticamente giustificate se per uscite anticipate o ritardi. Eliminazione bug di cancellazione giustifiche su reinserimento lezione.

--- lib/req_salva_assenze.php
+++ lib/req_salva_assenze.php
@@ -8,10 +8,13 @@ $idal = $id['al'];
 $asslez = "oreass" . $idal;
 
 $asslezal = stringa_html($asslez);
-
+$esisteassenza=false;
 //print "id al: $idal $asslez $asslezal <br>";
-$query = "delete from tbl_asslezione where idalunno=$idal and idlezione=$idlezione";
-eseguiQuery($con, $query);
+$query = "select * from tbl_asslezione where idalunno=$idal and idlezione=$idlezione";
+$ris= eseguiQuery($con, $query);
+if (mysqli_num_rows($ris)>0)
+    $esisteassenza=true;
+
 if ($asslezal != 0) {
     $flaguscent = false;
     // VERIFICO SE LA GIORNATA E' DI DAD E SE PER L'ALUNNO CI SONO RITARDI O USCITE
@@ -30,15 +33,24 @@ if ($asslezal != 0) {
         }
     }
 
+    if (!$esisteassenza)
+    {
     if (!$flaguscent)
         $query = "insert into tbl_asslezione(idalunno, idlezione,idmateria,data,oreassenza,forzata) values ($idal,$idlezione,$idmateria,'$data',$asslezal,true)";
     else
         $query = "insert into tbl_asslezione(idalunno, idlezione,idmateria,data,oreassenza,forzata,giustifica,iddocentegiust,datagiustifica) values ($idal,$idlezione,$idmateria,'$data',$asslezal,true,true,0,'$data')";
-
+    }
+    else
+        $query = "update tbl_asslezione set  oreassenza=$asslezal where idalunno=$idal and idlezione=$idlezione";
+  
     
     eseguiQuery($con, $query);
     
 }
-       
+else
+{
+    $query = "delete from tbl_asslezione where idalunno=$idal and idlezione=$idlezione";
+    eseguiQuery($con, $query);
+}
     
 
