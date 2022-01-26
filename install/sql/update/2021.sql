e9bf5aeac6a8f467f2507d9f87faa373d8659f3a
pietro <pietro@pietro-Aspire-A515-51G>
2022-01-26 16:38:30 +0100

    Assenze orarie automaticamente giustificate se per uscite anticipate o ritardi. Eliminazione bug di cancellazione giustifiche su reinserimento lezione.

--- install/sql/update/2021.sql
+++ install/sql/update/2021.sql
@@ -3,7 +3,7 @@
 --
 
 
-ALTER TABLE tbl_valutazioniobiettivi CHANGE perdiodo periodo char(1);
+-- ALTER TABLE tbl_valutazioniobiettivi CHANGE perdiodo periodo char(1);
 
 -- LASCIARE SEMPRE ALLA FINE
 UPDATE tbl_parametri set valore='2021' where parametro='versioneprecedente';
