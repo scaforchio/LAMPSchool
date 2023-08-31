1f88eea08cf1987c1b48b93831a7b6837224e8db
Vittorio Lo Mele <hi@vitto.dev>
2023-08-31 23:30:15 +0200

    .

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ if ($suffisso != "*")
 
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
         <p class="col-md-4 mb-0 text-muted">LampSchool ver. 2023</p> 
-            <a class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <a href='login/info.php' class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <img src="favicons/icon.png" alt="icona registro" class="bi me-2" height="40">
             </a>
         <ul class="nav col-md-4 justify-content-end">
