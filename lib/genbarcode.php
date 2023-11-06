92565f9901b021df935aaa7c32d77f9f3fb732dd
Vittorio Lo Mele <hi@vitto.dev>
2023-11-06 09:54:33 +0100

    2023.4 wip

--- lib/genbarcode.php
+++ lib/genbarcode.php
@@ -3,4 +3,4 @@ require '../vendor/autoload.php';
 
 header("Content-Type: image/svg+xml");
 $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
-echo $generator->getBarcode(isset($_GET["data"]) ? $_GET["data"] : "NULL" , $generator::TYPE_CODE_128, 3, 50);
+echo $generator->getBarcode(isset($_GET["data"]) ? $_GET["data"] : "NULL" , $generator::TYPE_CODE_128, 3, 100);
