75a59e7ec7579c7de466528dcc519bb940ce261a
Vittorio Lo Mele <hi@vitto.dev>
2023-11-07 10:40:16 +0100

    fix script aggiornamento

--- install/sql/update/2023.4.sql
+++ install/sql/update/2023.4.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `tbl_alunni` 
     ADD `telproprio` VARCHAR(20) NOT NULL DEFAULT '' after `idgrupporitardo`,
-    ADD `mailpropria` VARCHAR(100) NOT NULL DEFAULT '' after `telproprio`,
+    ADD `mailpropria` VARCHAR(100) NOT NULL DEFAULT '' after `telproprio`;
 
 -- LASCIARE SEMPRE ALLA FINE
 UPDATE tbl_parametri set valore='2023.4' where parametro='versioneprecedente';
